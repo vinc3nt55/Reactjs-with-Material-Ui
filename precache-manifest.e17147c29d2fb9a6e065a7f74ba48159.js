5c6c4f0b991f754f577e66f92e9d893b82135341
vinc3nt55 <vinc3nt55@gmail.com>
2019-02-07 16:51:28 +0800

    Updates

---
+++ precache-manifest.e17147c29d2fb9a6e065a7f74ba48159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0db3dbb585116733b56c",
+    "url": "/Reactjs-with-Material-Ui-and-Pixabay-API/static/js/runtime~main.0db3dbb5.js"
+  },
+  {
+    "revision": "0111892b39427d5a1436",
+    "url": "/Reactjs-with-Material-Ui-and-Pixabay-API/static/js/main.0111892b.chunk.js"
+  },
+  {
+    "revision": "1d784f128209a23f7cb0",
+    "url": "/Reactjs-with-Material-Ui-and-Pixabay-API/static/js/1.1d784f12.chunk.js"
+  },
+  {
+    "revision": "0111892b39427d5a1436",
+    "url": "/Reactjs-with-Material-Ui-and-Pixabay-API/static/css/main.a3aeaee1.chunk.css"
+  },
+  {
+    "revision": "b944b90e1881bf3160c31cedd48e8a12",
+    "url": "/Reactjs-with-Material-Ui-and-Pixabay-API/index.html"
+  }
+];
